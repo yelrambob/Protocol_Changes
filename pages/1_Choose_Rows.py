f8bfa5f82a0c468ed64a267851dc74d7226f1f59
yelrambob <sean.chinery@gmail.com>
2025-06-05 07:41:07 -0400

    Update 1_Choose_Rows.py

--- pages/1_Choose_Rows.py
+++ pages/1_Choose_Rows.py
@@ -44,7 +44,7 @@ for protocol in protocols:
     )
 
     columns = st.multiselect(
-        f"Select columns for {protocol}", options=list(df.columns), default=[df.columns[i] for i in [1, 3, 4, 7, 9, 10, 12, 14] if i < len(df.columns)], key=f"cols_{protocol}"
+        f"Select columns for {protocol}", options=list(df.columns), default=[df.columns[i] for i in [2, 4, 5, 8, 10, 11, 13, 15] if i < len(df.columns)], key=f"cols_{protocol}"
     )
 
     rename_dict = {}
